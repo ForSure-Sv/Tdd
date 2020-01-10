2cf0d3ec924ae10975ed6a688a36e76d95622351
Dan marinescuעןא <kaskason2@gmail.com>
2020-01-10 22:02:56 +0200

    added unittesting

--- bubbleSort.py
+++ bubbleSort.py
@@ -1,3 +1,5 @@
+import unittest
+
 def bubbleSort(arr):
     n = len(arr)
  
@@ -12,3 +14,32 @@ def bubbleSort(arr):
             # than the next element
             if arr[j] > arr[j+1] :
                 arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
+
+
+
+class BubbleSortCalcTesting(unittest.TestCase):
+    def test_bubbleSort(self):
+        # stub
+        arr1 = [4,3,2,1]
+        arr2 = [1]
+        arr3 = [0, 3, 5,-5,-2]
+
+        # assume
+        expected1 = [1, 2, 3, 4]
+        expected2 = [1]
+        expected3 = [-5, -2, 0, 3, 5]
+
+        # action
+        result1 = bubbleSort(arr1)
+        result2 = bubbleSort(arr2)
+        result3 = bubbleSort(arr3)
+
+        # expect/assert
+        self.assertEqual(result1, expected1)
+        self.assertEqual(result2, expected2)
+        self.assertEqual(result3, expected3)
+
+
+if __name__ == '__main__':
+    unittest.main()
